f9f9b21060b70aec2718f25555e8d4b835e1fbe4
Antonin Lindner <antoninlindner@gmail.com>
2026-02-17 12:44:09 +0000

    Adding analyses and some testing files

---
+++ Analysis/03-02-26_SCBA_SCS1_detail70K/analyse.py
@@ -0,0 +1,6 @@
+from utils import *
+import gc_utils.info as info
+from sc_break.analysis import analyse_scba, analyse_scba_indir
+
+analyse_scba_indir('data/data/')
+# analyse_scba('data/HS_reading_time=28-01-26_14-36-06.csv')
---
+++ Analysis/09-02-26_HS-MUX_comparison/analyse.py
@@ -0,0 +1,16 @@
+from utils import *
+# from hcl_pulse.parser import parse_config_string, get_hcl_pulse_data
+import gc_utils.info as info
+from current_rampup.parser import get_RIchar, get_RI_signal
+from current_rampup.plotter import plot_RI_characteristic, plot_RI_indir, plot_RI_signal, plot_RI_signal_indir
+from btc_log.plotter import plot_temp
+from hcl_pulse.analysis import sequence_analysis_indir, sequence_analysis
+
+# plot_RI_indir('data/')
+# plot_RI_signal_indir('data/')
+# plot_temp('data/BTC-log.csv', t_max='20/11/2025 18:00:00',to_plot=['Cold-head temp (K)', '4way cross temperature (K)'], savepath='data/BTC-log_temp_plot.png')
+
+sequence_analysis_indir('mount/old/')
+# sequence_analysis_indir('mount/new/')
+# sequence_analysis_indir('mount/new_fixed/')
+# sequence_analysis('data/sepdata/HCL-response_time=21-11-25_23-06-03.csv')
